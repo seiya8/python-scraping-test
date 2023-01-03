a9f6402c48cfe317dd3c95d51ab809c588cf0f97
seiya8 <seiya.111122@gmail.com>
2023-01-03 12:19:24 +0900

    サンプルコード微修正

--- PyChromeDevTools/sample_code.py
+++ PyChromeDevTools/sample_code.py
@@ -5,9 +5,9 @@ chrome = PyChromeDevTools.ChromeInterface()
 chrome.Network.enable()
 chrome.Page.enable()
 
-start_time=time.time()
-chrome.Page.navigate(url="http://www.google.com/")
+start_time = time.time()
+chrome.Page.navigate(url='https://www.google.com/')
 chrome.wait_event("Page.loadEventFired", timeout=60)
-end_time=time.time()
+end_time = time.time()
 
-print ("Page Loading Time:", end_time-start_time)
+print (f'Page Loading Time: {end_time-start_time} s')
